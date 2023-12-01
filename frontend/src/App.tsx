222e25b6edad6a360479c2f71045a05ffedce15c
AKOWSPC <akows141@gmail.com>
2023-12-01 21:05:54 +0900

    Init: 프로젝트의 상태를 관리할 Redux 라이브러리 설치. Init: Redux 사용을 위해 스토어 및 프로바이더를 설정.

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -4,27 +4,6 @@ import AboutPage from './pages/AboutPage';
 import HomePage from './pages/HomePage';
 
 function App() {
-
-  // 프론트엔드 -> 백엔드 -> 데이터베이스 연동 확인용 임시 기능.
-  // 각 파트의 연동이 정상 동작함을 확인하고 코드를 주석 처리함.
-  // const handleCreate = () => {
-  //   const testData = 'Test data';
-
-  //   axios.post('http://localhost:3001/api/create', { data: testData })
-  //     .then(response => {
-  //       console.log(response.data);
-  //       alert('Data created successfully');
-  //     })
-  //     .catch(error => {
-  //       console.error('Error creating data', error);
-  //       alert('Error creating data');
-  //     });
-  // };
-
-  // return (
-  //   <button onClick={handleCreate}>Create Test Data</button>
-  // );
-
   return (
     <BrowserRouter>
       <Routes>
@@ -35,8 +14,6 @@ function App() {
       </Routes>
     </BrowserRouter>
   );
-
-
 }
 
 export default App;
