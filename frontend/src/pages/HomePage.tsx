abbdb431fac79db1a44db34737ce929358c35022
AKOWSPC <akows141@gmail.com>
2023-12-03 20:46:00 +0900

    Feat: 마우스 스크롤 여부에 따른 애니매이션 기능 구현에 앞서, Intersection Observer API를 활용하여 뷰포트에 따라 특정 지점의 애니매이션이 작동하는 기능을 구현. Init: styled-components 라이브러리 설치, 프론트 - 백 - DB 연결 테스트용 코드를 미사용 컴포넌트로 이동.

--- frontend/src/pages/HomePage.tsx
+++ frontend/src/pages/HomePage.tsx
@@ -1,42 +1,56 @@
-import axios from 'axios';
-import React from 'react';
+import React, { useEffect, useRef, useState } from 'react';
+import styled from 'styled-components';
+
+// TextProps는 isVisible prop의 타입을 정의
+interface TextProps {
+  isVisible: boolean;
+}
+
+// Text 스타일 컴포넌트는 isVisible prop에 따라 투명도와 위치 변환을 적용
+const Text = styled.div<TextProps>`
+  opacity: ${({ isVisible }) => (isVisible ? 1 : 0)};
+  transform: translateY(${({ isVisible }) => (isVisible ? '0px' : '20px')});
+  transition: opacity 0.6s ease-out, transform 0.6s ease-out;
+  margin-bottom: 100vh; // 각 텍스트 요소 아래에 뷰포트 높이만큼 마진 추가
+`;
 
 const HomePage: React.FC = () => {
 
-  // 프론트엔드 -> 백엔드 -> 데이터베이스 연동 확인용 임시 기능.
-  // 각 파트의 연동이 정상 동작함을 확인하고 코드를 주석 처리함.
-  const handleCreate = () => {
-    const testData = 'Test data';
+  // isVisible 상태는 각 텍스트의 가시성을 관리
+  const [isVisible, setIsVisible] = useState({ text1: false, text2: false, text3: false });
+  
+  // 각 텍스트 요소를 참조하기 위한 ref
+  const text1Ref = useRef(null);
+  const text2Ref = useRef(null);
+  const text3Ref = useRef(null);
 
-    axios.post('http://3.36.128.6:3001/api/create', { data: testData })
-      .then(response => {
-        console.log(response.data);
-        alert('Data created successfully');
-      })
-      .catch(error => {
-        console.error('Error creating data', error);
-        alert('Error creating data');
+  useEffect(() => {
+    // Intersection Observer는 각 요소가 뷰포트에 들어오는지 감지
+    const observer = new IntersectionObserver(entries => {
+      entries.forEach(entry => {
+        // 각 요소가 뷰포트에 들어오면 isVisible 상태 업데이트
+        setIsVisible(prev => ({ ...prev, [entry.target.id]: entry.isIntersecting }));
       });
-  };
-
-  const handleFetchData = () => {
-    axios.get('http://3.36.128.6:3001/api/getAll')
-      .then(response => {
-        console.log(response.data);
-        alert('Data fetched successfully');
-      })
-      .catch(error => {
-        console.error('Error fetching data', error);
-        alert('Error fetching data');
-      });
-  };
+    }, { threshold: 0.5 }); // 요소가 뷰포트의 50% 보일 때 트리거
+  
+    // 각 ref가 실제 DOM 요소를 가리키고 있으면 observer에 등록
+    if (text1Ref.current) observer.observe(text1Ref.current);
+    if (text2Ref.current) observer.observe(text2Ref.current);
+    if (text3Ref.current) observer.observe(text3Ref.current);
+  
+    // 컴포넌트 언마운트 시 observer 연결 해제
+    return () => {
+      observer.disconnect();
+    };
+  }, []);
+  
 
   return (
     <div>
-      <h1>향수 온라인 쇼핑몰에 오신 것을 환영합니다!</h1>
-      <p>다양한 향수를 탐색하고, 새로운 향기를 경험해 보세요.</p>
-      <button onClick={handleCreate}>Create Test Data</button>
-      <button onClick={handleFetchData}>Fetch Test Data</button>
+      {/* 각 Text 컴포넌트는 ref와 isVisible 상태를 props로 받아 렌더링 */}
+      <Text ref={text1Ref} id="text1" isVisible={isVisible.text1}>Text 1</Text>
+      <Text ref={text2Ref} id="text2" isVisible={isVisible.text2}>Text 2</Text>
+      <Text ref={text3Ref} id="text3" isVisible={isVisible.text3}>Text 3</Text>
     </div>
   );
 };
