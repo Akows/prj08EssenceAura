df175920f1e36dd3811e3bc08afc0aa5256ab475
AKOWSPC <akows141@gmail.com>
2023-12-12 15:37:17 +0900

    Design: Footer 컴포넌트의 디자인 및 기능 정리. (테마, 언어 변경 기능을 Footer에서 Nav로 다시 이관.)

--- frontend/src/components/common/Footer.tsx
+++ frontend/src/components/common/Footer.tsx
@@ -1,7 +1,5 @@
 import React from 'react';
 import styled from 'styled-components';
-import { useChangeLanguage } from '../../hooks/useChangeLanguage';
-import { useChangeTheme } from '../../hooks/useChangeTheme';
 
 const FooterContainer = styled.footer`
   background-color: #333;
@@ -23,65 +21,9 @@ const FooterText = styled.p`
   transform: translateX(-50%);
 `;
 
-const Dropdown = styled.div`
-  position: relative;
-  display: inline-block;
-`;
-
-const DropdownContent = styled.div`
-  display: none;
-  position: absolute;
-  background-color: #f9f9f9;
-  min-width: 160px;
-  box-shadow: 0px 8px 16px 0px rgba(0,0,0,0.2);
-  z-index: 1;
-  color: black;
-  left: 0;
-
-  & button {
-    color: black;
-    padding: 12px 16px;
-    text-decoration: none;
-    display: block;
-    border: none;
-    background: none;
-    text-align: left;
-
-    &:hover {
-      background-color: #f1f1f1;
-    }
-  }
-`;
-
-const DropdownButton = styled.button`
-  background-color: transparent;
-  border: 1px solid white;
-  color: white;
-  padding: 5px 10px;
-  cursor: pointer;
-
-  &:hover {
-    ${DropdownContent} {
-      display: block;
-    }
-  }
-`;
-
 const Footer: React.FC = () => {
-  const changeTheme = useChangeTheme();
-  const changeLanguage = useChangeLanguage();
-
   return (
     <FooterContainer>
-      <Dropdown>
-        <DropdownButton>설정</DropdownButton>
-        <DropdownContent>
-          <button onClick={() => changeLanguage('en')}>EN</button>
-          <button onClick={() => changeLanguage('ko')}>KO</button>
-          <button onClick={() => changeTheme('dark')}>Dark</button>
-          <button onClick={() => changeTheme('light')}>Light</button>
-        </DropdownContent>
-      </Dropdown>
       <FooterText>EssenceAura</FooterText>
     </FooterContainer>
   );
