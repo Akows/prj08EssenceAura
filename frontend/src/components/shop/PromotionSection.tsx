f24b8bb3f8425187e75cb6edd741412a7bd4e178
AKOWSPC <akows141@gmail.com>
2023-12-29 23:56:04 +0900

    Design: 쇼핑몰 메인 페이지, 프로모션 섹션 UI 1차 구현.

---
+++ frontend/src/components/shop/PromotionSection.tsx
@@ -0,0 +1,55 @@
+import React from 'react';
+import styled from 'styled-components';
+
+const PromotionContainer = styled.section`
+    display: flex;
+    flex-wrap: wrap;
+    justify-content: center;
+    gap: 20px; // 각 프로모션 항목 사이의 간격
+    margin: 40px 0; // 상하 여백
+`;
+
+const PromotionItem = styled.div`
+    width: 300px; // 각 프로모션 항목의 너비
+    box-shadow: 0 2px 5px rgba(0, 0, 0, 0.2); // 그림자 효과
+    text-align: center; // 텍스트 중앙 정렬
+    padding: 20px;
+    background: #fff; // 배경색
+
+    img {
+        width: 100%;
+        height: auto;
+        object-fit: cover;
+    }
+
+    h3 {
+        margin: 10px 0;
+    }
+`;
+
+const promotions = [
+    {
+        id: 1,
+        title: 'Winter Event',
+        imageUrl: '/path/to/promotion1.jpg',
+        description: '새해맞이 할인 이벤트 진행 중!',
+    },
+    // ... 여기에 다른 프로모션 항목 추가
+];
+
+const PromotionSection: React.FC = () => {
+    return (
+        <PromotionContainer>
+            {promotions.map((promo) => (
+                <PromotionItem key={promo.id}>
+                    <img src={promo.imageUrl} alt={promo.title} />
+                    <h3>{promo.title}</h3>
+                    <p>{promo.description}</p>
+                    {/* 필요한 경우 여기에 버튼 추가 */}
+                </PromotionItem>
+            ))}
+        </PromotionContainer>
+    );
+};
+
+export default PromotionSection;
