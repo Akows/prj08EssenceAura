a9179ccd794b2032b978fdb3b55baed39455c315
AKOWSPC <akows141@gmail.com>
2023-12-30 15:33:51 +0900

    Design: 하단 푸터 컴포넌트 구현.

--- frontend/src/components/common/Footer.tsx
+++ frontend/src/components/common/Footer.tsx
@@ -7,24 +7,44 @@ const FooterContainer = styled.footer`
     padding: 20px;
     text-align: center;
     display: flex;
-    justify-content: space-between;
-    align-items: center;
-    box-sizing: border-box; // 페이지 모서리로부터 떨어지지 않도록
+    flex-direction: column; // 세로 정렬
+    align-items: center; // 가로축에서 중앙 정렬
+    box-sizing: border-box;
 `;
 
-const FooterText = styled.p`
-    margin: 0;
-    padding: 0;
-    font-size: 20px;
-    position: absolute; // 중앙에 위치
-    left: 50%;
-    transform: translateX(-50%);
+const FooterTop = styled.div`
+    width: 100%;
+    display: flex;
+    justify-content: space-evenly; // 항목들을 균등하게 분포
+    margin-bottom: 20px; // 하단 텍스트와의 여백
+`;
+
+const FooterLink = styled.a`
+    color: white;
+    text-decoration: none;
+
+    &:hover {
+        text-decoration: underline;
+    }
+`;
+
+const FooterBottom = styled.div`
+    font-size: 0.8em;
 `;
 
 const Footer: React.FC = () => {
     return (
         <FooterContainer>
-            <FooterText>EssenceAura</FooterText>
+            <FooterTop>
+                <FooterLink href="#">사이트맵</FooterLink>
+                <FooterLink href="#">개인정보 처리방침</FooterLink>
+                <FooterLink href="#">이용 약관</FooterLink>
+                <FooterLink href="#">연락처</FooterLink>
+            </FooterTop>
+            <FooterBottom>
+                &copy; {new Date().getFullYear()} EssenceAura. All rights
+                reserved.
+            </FooterBottom>
         </FooterContainer>
     );
 };
