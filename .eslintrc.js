3aef514d27e6792c88b10288785d2c7ccabef5b7
AKOWSPC <akows141@gmail.com>
2024-01-01 14:35:44 +0900

    Fix: ESLint상의 들여쓰기 규칙과 SwitchCase의 들여쓰기 규칙이 충돌하여 지속적으로 코드상의 에러 문구를 출력하는 문제를 해결. (ESLint의 들여쓰기 규칙에 예외 규칙을 추가하여 해결.)

--- .eslintrc.js
+++ .eslintrc.js
@@ -4,7 +4,8 @@ module.exports = {
       'plugin:@typescript-eslint/recommended', // TypeScript 규칙 사용
       'plugin:react/recommended', // React 규칙 사용
       'plugin:react-hooks/recommended', // React Hooks 규칙 사용
-      'plugin:prettier/recommended' // Prettier 규칙 사용
+      'plugin:prettier/recommended', // Prettier 규칙 사용
+      'eslint-config-prettier', // eslint-config-prettier를 여기에 추가
     ],
     settings: {
       react: {
@@ -32,5 +33,8 @@ module.exports = {
         '@typescript-eslint/explicit-function-return-type': 'off', // 함수 반환 타입 명시적 선언 비활성화
         '@typescript-eslint/no-explicit-any': 'warn', // 'any' 타입 사용에 대해 경고
       },
+      // 들여쓰기 규칙 설정.
+      // 기본 들여쓰기 레벨은 4, 다만 SwitchCase의 경우 추가로 4 * 1만큼의 추가 들여쓰기 적용.
+      'indent': ['error', 4, { 'SwitchCase': 1 }],
   };
   
