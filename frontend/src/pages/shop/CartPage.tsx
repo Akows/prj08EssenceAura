922f81a37fc78274434189b6f0c20fb3ff459ea5
AKOWSPC <akows141@gmail.com>
2024-01-03 15:16:50 +0900

    Design: 장바구니 페이지 UI 수정. (반응형 웹 대응, 모바일 환경에서 페이지 요소들이 문제없이 출력되도록 대응) (페이지 구성 요소들이 너무 좁게 출력되는 모습을 개선.) (이동 버튼의 Link 태그 영역의 가로세로 넒이가 제어되지 않는 문제가 발생함. 해결 예정.)

--- frontend/src/pages/shop/CartPage.tsx
+++ frontend/src/pages/shop/CartPage.tsx
@@ -1,9 +1,17 @@
 import React, { useState } from 'react';
+import { Link } from 'react-router-dom';
 import styled from 'styled-components';
 import CartItem from '../../components/shop/CartItem';
 
 const CartContainer = styled.div`
     padding: 20px;
+    width: 100%; // 화면 전체 폭을 사용하도록 변경
+    margin: 10px 40px; // 좌우 중앙 정렬을 위해 변경
+
+    @media (max-width: 768px) {
+        margin: 20px 20px; // 모바일 환경에서의 마진 조정
+        padding: 10px; // 모바일 환경에서의 패딩 조정
+    }
 `;
 
 const Breadcrumb = styled.div`
@@ -14,6 +22,7 @@ const Breadcrumb = styled.div`
 
 const CartTitle = styled.h1`
     font-size: 24px;
+    color: #333;
     border-bottom: 1px solid #ccc;
     padding-bottom: 10px;
 `;
@@ -22,12 +31,18 @@ const CartTable = styled.table`
     width: 100%;
     margin-top: 20px;
     border-collapse: collapse;
+
+    @media (max-width: 768px) {
+        // 모바일 환경에서 테이블 스타일 조정
+        // 예를 들어, 폰트 크기, 셀 패딩 등
+    }
 `;
 
 const CartHeader = styled.th`
     text-align: left;
     padding: 8px;
     background: #f7f7f7;
+    color: #333;
     border-bottom: 1px solid #ccc;
 `;
 
@@ -35,18 +50,38 @@ const TotalPrice = styled.div`
     text-align: right;
     margin-top: 20px;
     font-size: 20px;
+    color: #e44d26;
 `;
 
 const Button = styled.button`
-    padding: 10px 20px;
-    margin-right: 10px;
+    width: 135px;
+    height: 50px;
+    background-color: #e44d26; // 버튼 배경 색상 변경
     border: none;
-    background-color: #000;
-    color: #fff;
+    padding: 10px 15px;
     cursor: pointer;
+    margin-right: 10px;
+
+    & > a {
+        text-decoration: none;
+        color: white;
+        font-size: 16px;
+        border: 2px black solid;
+    }
+
+    &:hover {
+        background-color: #f55f3b; // 호버 효과 색상 변경
+    }
 
     &:last-child {
         margin-right: 0;
+    }
+
+    // 모바일 환경에서 버튼 스타일 조정
+    @media (max-width: 768px) {
+        width: 100%; // 모바일 화면에서 버튼의 폭을 전체로 확장
+        margin-top: 10px; // 버튼 사이의 마진 조정
+        margin-right: 0; // 버튼 사이의 오른쪽 마진 제거
     }
 `;
 
@@ -119,9 +154,12 @@ const CartPage: React.FC = () => {
             </CartTable>
             <TotalPrice>총 구매금액: 370,000원</TotalPrice>
             <div>
-                <Button>주문하기</Button>
-                <Button>계속 쇼핑하기</Button>
-                <Button>결제하기</Button>
+                <Button>
+                    <Link to="/shop">계속 쇼핑하기</Link>
+                </Button>
+                <Button>
+                    <Link to="/checkout">결제하기</Link>
+                </Button>
             </div>
         </CartContainer>
     );
