e4c0fc59c53d1414b2161c5172bc8a2d08a7e287
AKOWSPC <akows141@gmail.com>
2024-01-11 20:56:54 +0900

    Fix: Cookie 로직 추가 이후 발생하는 CORS 문제 해결. (백엔드에서 CORS 미들웨어를 사용 중이었으나, 모든 주소에서 오는 요청을 허가하는 기존 설정이 문제의 원인.) (Cookie 사용 허가 설정시에는 이게 불가능하다고 함..) (따라서 미들웨어 설정에서 특정 URL에서의 요청만 허가하도록 하고, 쿠키 허용 옵션도 추가함.)

--- backend/src/app.js
+++ backend/src/app.js
@@ -3,7 +3,15 @@ const cors = require('cors');
 const userRoutes = require('./routes/userRoutes');
 const app = express();
 
-app.use(cors());
+// CORS 미들웨어 설정
+const corsOptions = {
+    origin: 'http://localhost:5173',
+    credentials: true, // 이 옵션을 통해 쿠키를 함께 보낼 수 있도록 허가
+    methods: 'GET, HEAD, PUT, PATCH, POST, DELETE',
+    allowedHeaders: ['Content-Type', 'Authorization'],
+};
+
+app.use(cors(corsOptions));
 app.use(express.json());
 
 // 사용자 라우트 추가
