90856afb14f779deba573c0cc415f6e47a861c99
AKOWSPC <akows141@gmail.com>
2024-01-23 23:10:21 +0900

    Feat: 백엔드 에러처리 로직 구현. (컨트롤러, 서비스 레이어에서 중구난방식으로 처리되던 에러를 에러 클래스를 통해 한 곳에서 일괄처리 할 수 있도록 구현.) (컨트롤러와 서비스 레이어에서 직접 처리해야하는 에러는 그것대로 적절하게 대응할 수 있도록 로직을 설계함.)

--- backend/src/controllers/adminController.js
+++ backend/src/controllers/adminController.js
@@ -6,7 +6,11 @@ const getAllUsersHandler = async (req, res) => {
         const users = await getAllUsers();
         res.json(users);
     } catch (error) {
-        res.status(500).send({ message: '유저 정보 조회 중 오류가 발생했습니다.' });
+        if (error instanceof DatabaseError) {
+            res.status(500).send({ message: error.message });
+        } else {
+            res.status(500).send({ message: '유저 정보 조회 중, 서버에서 오류가 발생하였습니다.' });
+        }
     }
 };
 
@@ -18,12 +22,15 @@ const searchUserByEmailHandler = async (req, res) => {
     }
     try {
         const users = await searchUserByEmail(emailKeyword);
-        if (users.length === 0) {
-            return res.status(404).send({ message: '검색 결과가 없습니다.' });
-        }
         res.json(users);
     } catch (error) {
-        res.status(500).send({ message: '이메일로 유저 검색 중 오류가 발생했습니다.' });
+        if (error instanceof NotFoundError) {
+            res.status(404).send({ message: error.message });
+        } else if (error instanceof DatabaseError) {
+            res.status(500).send({ message: error.message });
+        } else {
+            res.status(500).send({ message: '유저 정보 검색 중, 서버에서 오류가 발생하였습니다.' });
+        }
     }
 };
 
@@ -33,102 +40,141 @@ const updateUserHandler = async (req, res) => {
         await updateUser(req.params.id, req.body);
         res.send({ message: '유저 정보가 성공적으로 업데이트되었습니다.' });
     } catch (error) {
-        res.status(500).send({ message: '유저 정보 업데이트 중 오류가 발생했습니다.' });
+        if (error instanceof DatabaseError) {
+            res.status(500).send({ message: error.message });
+        } else {
+            res.status(500).send({ message: '유저 정보 수정 중, 서버에서 오류가 발생하였습니다.' });
+        }
     }
 };
 
-// 유저 비활성화 (is_active = false)
+// 유저 비활성화 핸들러
 const deactivateUserHandler = async (req, res) => {
     try {
         await deactivateUser(req.params.id);
         res.send({ message: '유저가 성공적으로 비활성화되었습니다.' });
     } catch (error) {
-        res.status(500).send({ message: '유저 비활성화 중 오류가 발생했습니다.' });
+        if (error instanceof DatabaseError) {
+            res.status(500).send({ message: error.message });
+        } else {
+            res.status(500).send({ message: '유저 정보 비활성화 중, 서버에서 오류가 발생하였습니다.' });
+        }
     }
 };
 
-// 관리자 전체 조회
+// 관리자 전체 조회 핸들러
 const getAllAdminsHandler = async (req, res) => {
     try {
         const admins = await getAllAdmins();
         res.json(admins);
     } catch (error) {
-        res.status(500).send({ message: '관리자 정보 조회 중 오류가 발생했습니다.' });
+        if (error instanceof DatabaseError) {
+            res.status(500).send({ message: error.message });
+        } else {
+            res.status(500).send({ message: '관리자 정보 조회 중, 서버에서 오류가 발생하였습니다.' });
+        }
     }
 };
 
-// 관리자 추가
+// 관리자 추가 핸들러
 const createAdminHandler = async (req, res) => {
     try {
         await createAdmin(req.body);
-        res.status(201);
+        res.status(201).send({ message: '관리자가 성공적으로 추가되었습니다.' });
     } catch (error) {
-        if (error.message === '이미 사용 중인 이메일 주소입니다.') {
-            return res.status(409).send({ message: error.message }); // 409 Conflict
+        if (error instanceof ResourceConflictError) {
+            res.status(409).send({ message: error.message });
+        } else if (error instanceof DatabaseError) {
+            res.status(500).send({ message: error.message });
+        } else {
+            res.status(500).send({ message: '관리자 정보 추가 중, 서버에서 오류가 발생하였습니다.' });
         }
-        res.status(500).send({ message: '관리자 추가 중 오류가 발생했습니다.' });
     }
 };
 
-// 관리자 정보 수정
+// 관리자 정보 수정 핸들러
 const updateAdminHandler = async (req, res) => {
     try {
         await updateAdmin(req.params.id, req.body);
         res.send({ message: '관리자 정보가 성공적으로 업데이트되었습니다.' });
     } catch (error) {
-        res.status(500).send({ message: '관리자 정보 수정 중 오류가 발생했습니다.' });
+        if (error instanceof DatabaseError) {
+            res.status(500).send({ message: error.message });
+        } else {
+            res.status(500).send({ message: '관리자 정보 수정 중, 서버에서 오류가 발생하였습니다.' });
+        }
     }
 };
 
-// 관리자 삭제
+// 관리자 삭제 핸들러
 const deleteAdminHandler = async (req, res) => {
     try {
         await deleteAdmin(req.params.id);
         res.send({ message: '관리자가 성공적으로 삭제되었습니다.' });
     } catch (error) {
-        res.status(500).send({ message: '관리자 삭제 중 오류가 발생했습니다.' });
+        if (error instanceof DatabaseError) {
+            res.status(500).send({ message: error.message });
+        } else {
+            res.status(500).send({ message: '관리자 정보 삭제 중, 서버에서 오류가 발생하였습니다.' });
+        }
     }
 };
 
-// 상품 목록을 가져오는 컨트롤러 함수
+
+
+// 상품 목록을 가져오는 핸들러
 const getProductsHandler = async (req, res) => {
     try {
         const products = await getProducts();
         res.json({ success: true, data: products });
     } catch (error) {
-        res.status(500).json({ success: false, message: error.message });
+        if (error instanceof DatabaseError) {
+            res.status(500).json({ success: false, message: error.message });
+        } else {
+            res.status(500).json({ success: false, message: '관리자 상품 정보 조회 중, 서버에서 오류가 발생하였습니다.' });
+        }
     }
 };
 
-// 상품을 추가하는 컨트롤러 함수
+// 상품을 추가하는 핸들러
 const addProductHandler = async (req, res) => {
     try {
         const newProduct = await addProduct(req.body);
         res.status(201).json({ success: true, data: newProduct });
     } catch (error) {
-        res.status(500).json({ success: false, message: error.message });
+        if (error instanceof DatabaseError) {
+            res.status(500).json({ success: false, message: error.message });
+        } else {
+            res.status(500).json({ success: false, message: '관리자 상품 정보 추가 중, 서버에서 오류가 발생하였습니다.' });
+        }
     }
 };
 
-// 상품 정보를 수정하는 컨트롤러 함수
+// 상품 정보를 수정하는 핸들러
 const updateProductHandler = async (req, res) => {
-    const { id } = req.params;
     try {
-        const updatedProduct = await updateProduct(id, req.body);
+        const updatedProduct = await updateProduct(req.params.id, req.body);
         res.json({ success: true, data: updatedProduct });
     } catch (error) {
-        res.status(500).json({ success: false, message: error.message });
+        if (error instanceof DatabaseError) {
+            res.status(500).json({ success: false, message: error.message });
+        } else {
+            res.status(500).json({ success: false, message: '관리자 상품 정보 수정 중, 서버에서 오류가 발생하였습니다.' });
+        }
     }
 };
 
-// 상품을 삭제하는 컨트롤러 함수
+// 상품을 삭제하는 핸들러
 const deleteProductHandler = async (req, res) => {
-    const { id } = req.params;
     try {
-        await deleteProduct(id);
+        await deleteProduct(req.params.id);
         res.json({ success: true, message: '상품이 삭제되었습니다.' });
     } catch (error) {
-        res.status(500).json({ success: false, message: error.message });
+        if (error instanceof DatabaseError) {
+            res.status(500).json({ success: false, message: error.message });
+        } else {
+            res.status(500).json({ success: false, message: '관리자 상품 정보 삭제 중, 서버에서 오류가 발생하였습니다.' });
+        }
     }
 };
 
--- backend/src/service/adminService.js
+++ backend/src/service/adminService.js
@@ -1,5 +1,6 @@
 const db = require('../config/database');
 const bcrypt = require('bcrypt');
+const { DatabaseError, NotFoundError, ResourceConflictError } = require('../error/error');
 
 // 모든 유저 정보 조회
 const getAllUsers = async () => {
@@ -7,20 +8,27 @@ const getAllUsers = async () => {
         const [users] = await db.query('SELECT user_id, username, email, address, phone_number, created_at, is_active FROM users WHERE is_active = 1');
         return users;
     } catch (error) {
-        throw new Error('유저 정보 조회 중 데이터베이스 오류가 발생했습니다.');
+        throw new DatabaseError('유저 정보 조회 중 오류 발생');
     }
 };
 
 // 특정 유저 정보 검색
 const searchUserByEmail = async (emailKeyword) => {
     try {
-        // '%' 와일드카드를 사용하여 부분 일치 검색을 합니다.
+        // '%' 와일드카드를 사용하여 부분 일치 검색.
         const query = `SELECT * FROM users WHERE email LIKE ? AND is_active = 1`;
         const values = [`%${emailKeyword}%`];
         const [users] = await db.query(query, values);
+
+        if (users.length === 0) {
+            throw new NotFoundError('검색 결과가 없습니다.');
+        }
         return users;
     } catch (error) {
-        throw new Error('이메일로 유저 검색 중 데이터베이스 오류가 발생했습니다.');
+        if (error instanceof NotFoundError) {
+            throw error;
+        }
+        throw new DatabaseError('이메일로 유저 검색 중 오류 발생');
     }
 };
 
@@ -29,7 +37,7 @@ const updateUser = async (userId, userData) => {
     try {
         await db.query('UPDATE users SET ? WHERE user_id = ?', [userData, userId]);
     } catch (error) {
-        throw new Error('유저 정보 업데이트 중 데이터베이스 오류가 발생했습니다.');
+        throw new DatabaseError('유저 정보 업데이트 중 오류 발생');
     }
 };
 
@@ -38,7 +46,7 @@ const deactivateUser = async (userId) => {
     try {
         await db.query('UPDATE users SET is_active = 0 WHERE user_id = ?', [userId]);
     } catch (error) {
-        throw new Error('유저 비활성화 중 데이터베이스 오류가 발생했습니다.');
+        throw new DatabaseError('유저 비활성화 중 데이터베이스 오류가 발생했습니다.');
     }
 };
 
@@ -48,25 +56,27 @@ const getAllAdmins = async () => {
         const [admins] = await db.query('SELECT admin_id, username, email, created_at FROM admins');
         return admins;
     } catch (error) {
-        throw new Error('관리자 정보 조회 중 데이터베이스 오류가 발생했습니다.');
+        throw new DatabaseError('관리자 정보 조회 중 오류 발생');
     }
 };
 
 // 관리자 추가
 const createAdmin = async (adminData) => {
-    // 먼저 데이터베이스에서 이메일 주소가 이미 사용 중인지 확인합니다.
-    const [existingAdmins] = await db.query('SELECT * FROM admins WHERE email = ?', [adminData.email]);
-    if (existingAdmins.length > 0) {
-        throw new Error('이미 사용 중인 이메일 주소입니다.');
-    }
-
-    // 이메일 주소가 중복되지 않았다면, 관리자 계정을 생성합니다.
-    const hashedPassword = await bcrypt.hash(adminData.password, 10);
     try {
-        const [result] = await db.query('INSERT INTO admins (username, email, password, created_at, updated_at) VALUES (?, ?, ?, NOW(), NOW());', [adminData.username, adminData.email, hashedPassword]);
-        await db.query('SELECT * FROM admins WHERE admin_id = ?', [result.insertId]);
+        // 먼저 데이터베이스에서 이메일 주소가 이미 사용 중인지 확인.
+        const [existingAdmins] = await db.query('SELECT * FROM admins WHERE email = ?', [adminData.email]);
+        if (existingAdmins.length > 0) {
+            throw new ResourceConflictError('이미 사용 중인 이메일 주소입니다.');
+        }
+
+        // 이메일 주소가 중복되지 않았다면, 관리자 계정을 생성.
+        const hashedPassword = await bcrypt.hash(adminData.password, 10);
+        await db.query('INSERT INTO admins (username, email, password, created_at, updated_at) VALUES (?, ?, ?, NOW(), NOW());', [adminData.username, adminData.email, hashedPassword]);
     } catch (error) {
-        throw new Error('관리자 추가 중 데이터베이스 오류가 발생했습니다.');
+        if (error instanceof ResourceConflictError) {
+            throw error;
+        }
+        throw new DatabaseError('관리자 추가 중 오류 발생');
     }
 };
 
@@ -75,7 +85,7 @@ const updateAdmin = async (adminId, adminData) => {
     try {
         await db.query('UPDATE admins SET ? WHERE admin_id = ?', [adminData, adminId]);
     } catch (error) {
-        throw new Error('관리자 정보 수정 중 데이터베이스 오류가 발생했습니다.');
+        throw new DatabaseError('관리자 정보 수정 중 오류 발생');
     }
 };
 
@@ -84,53 +94,49 @@ const deleteAdmin = async (adminId) => {
     try {
         await db.query('DELETE FROM admins WHERE admin_id = ?', [adminId]);
     } catch (error) {
-        throw new Error('관리자 삭제 중 데이터베이스 오류가 발생했습니다.');
+        throw new DatabaseError('관리자 삭제 중 오류 발생');
     }
 };
 
-// 상품 목록을 가져오는 서비스 함수
+// 상품 가져오기
 const getProducts = async () => {
     try {
-        // 데이터베이스 쿼리를 실행하여 상품 목록을 가져온다.
         const products = await db.query('SELECT * FROM products');
         return products;
     } catch (error) {
-        throw error;
+        throw new DatabaseError('상품 정보 조회 중 오류 발생');
     }
 };
 
-// 상품을 추가하는 서비스 함수
+
+// 상품 추가
 const addProduct = async (productData) => {
     try {
-        // 새 상품 데이터를 데이터베이스에 추가.
         const result = await db.query('INSERT INTO products SET ?', productData);
         return { id: result.insertId, ...productData };
     } catch (error) {
-        throw error;
+        throw new DatabaseError('상품 추가 중 오류 발생');
     }
 };
 
-// 상품 정보를 수정하는 서비스 함수
+// 상품 정보 수정
 const updateProduct = async (id, productData) => {
     try {
-        // 상품 ID를 기준으로 상품 정보를 업데이트.
         await db.query('UPDATE products SET ? WHERE product_id = ?', [productData, id]);
         return { id, ...productData };
     } catch (error) {
-        throw error;
+        throw new DatabaseError('상품 정보 수정 중 오류 발생');
     }
 };
 
-// 상품을 삭제하는 서비스 함수
+// 상품 삭제
 const deleteProduct = async (id) => {
     try {
-        // 상품 ID를 기준으로 상품을 삭제.
         await db.query('DELETE FROM products WHERE product_id = ?', id);
     } catch (error) {
-        throw error;
+        throw new DatabaseError('상품 삭제 중 오류 발생');
     }
 };
-
 
 // 내보내기
 module.exports = {
