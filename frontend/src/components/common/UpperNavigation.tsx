6227b89ae67862bcc9eda92765d0bd883b7c8660
AKOWSPC <akows141@gmail.com>
2024-03-01 21:21:52 +0900

    Design: 추천 검색어 컴포넌트의 영역을 데스크톱 환경에서 페이지 중앙에 나타나도록 수정.
    Design: 프로젝트 탭 이미지 및 타이틀 설정, 링크 섬네일과 텍스트를 설정함.

--- frontend/src/components/common/UpperNavigation.tsx
+++ frontend/src/components/common/UpperNavigation.tsx
@@ -138,18 +138,22 @@ const SearchButton = styled.button`
 const SearchSuggestionsContainer = styled.div`
     display: ${(props) => (props.show ? 'block' : 'none')};
     position: absolute;
-    top: 100%;
-    left: 0;
-    right: 0;
+    top: 100%; // 기존 위치 유지
+    left: 30%; // 중앙 정렬을 위해 수정
+    right: 25%; // 중앙 정렬을 위해 수정
     background-color: white;
     box-shadow: 0px 4px 6px rgba(0, 0, 0, 0.1);
     border-radius: 0 0 4px 4px;
     overflow-y: auto; // 내용이 넘칠 경우 세로 스크롤바 생성
     max-height: 300px; // 최대 높이 설정
     z-index: 10;
+    width: 50%; // 너비를 50%로 설정
 
     @media (max-width: 768px) {
         top: 38px;
+        left: 0; // 모바일 화면에서는 기존 설정 유지
+        right: 0; // 모바일 화면에서는 기존 설정 유지
+        width: 100%; // 모바일 화면에서는 전체 너비 사용
     }
 `;
 
@@ -446,23 +450,23 @@ const UpperNavigation: React.FC = () => {
                         <SearchSuggestionsContainer show={showSuggestions}>
                             {hasSuggestions
                                 ? suggestions.map((suggestion, index) => (
-                                      <SuggestionItem
-                                          key={`${suggestion.type}-${index}`}
-                                          onClick={() =>
-                                              onSuggestionClick(
-                                                  suggestion.value,
-                                                  suggestion.type
-                                              )
-                                          }
-                                      >
-                                          {suggestion.value}
-                                      </SuggestionItem>
-                                  ))
-                                : showSuggestions && (
-                                      <SuggestionItem>
-                                        검색 결과가 존재하지 않습니다.
+                                    <SuggestionItem
+                                        key={`${suggestion.type}-${index}`}
+                                        onClick={() =>
+                                            onSuggestionClick(
+                                                suggestion.value,
+                                                suggestion.type
+                                            )
+                                        }
+                                    >
+                                        {suggestion.value}
                                     </SuggestionItem>
-                                )}
+                                ))
+                                : showSuggestions && (
+                                    <SuggestionItem>
+                                          검색 결과가 존재하지 않습니다.
+                                    </SuggestionItem>
+                                  )}
                         </SearchSuggestionsContainer>
                     </SearchBarContainer>
 
@@ -549,25 +553,25 @@ const UpperNavigation: React.FC = () => {
                                 >
                                     {hasSuggestions
                                         ? suggestions.map(
-                                              (suggestion, index) => (
-                                                  <SuggestionItem
-                                                      key={`${suggestion.type}-${index}`}
-                                                      onClick={() =>
-                                                        onSuggestionClick(
-                                                            suggestion.value,
-                                                              suggestion.type
-                                                          )
-                                                    }
-                                                >
-                                                      {suggestion.value}
-                                                </SuggestionItem>
-                                              )
-                                        )
+                                            (suggestion, index) => (
+                                                <SuggestionItem
+                                                    key={`${suggestion.type}-${index}`}
+                                                    onClick={() =>
+                                                          onSuggestionClick(
+                                                              suggestion.value,
+                                                            suggestion.type
+                                                        )
+                                                      }
+                                                  >
+                                                    {suggestion.value}
+                                                  </SuggestionItem>
+                                            )
+                                          )
                                         : showSuggestions && (
-                                              <SuggestionItem>
-                                                검색 결과가 존재하지 않습니다.
+                                            <SuggestionItem>
+                                                  검색 결과가 존재하지 않습니다.
                                               </SuggestionItem>
-                                          )}
+                                        )}
                                 </SearchSuggestionsContainer>
                             </MobileSearchBarContainer>
                             {isLoggedIn ? (
